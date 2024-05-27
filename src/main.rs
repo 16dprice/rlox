15bdc2291a46bef31832133008bfd3e3b282acc9
DJ <djpric91@gmail.com>
2024-05-26 19:56:58 -0500

    Add ability to read file

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ mod value;
 
 use chunk::{Chunk, OpCode};
 use debug::disassemble_chunk;
-use std::io::{self, Write};
+use std::fs::File;
+use std::io::{self, Read, Write};
 
-fn main() {
+fn repl() {
     loop {
         print!("> ");
         io::stdout().flush().unwrap();
@@ -21,5 +22,28 @@ fn main() {
         if input.eq_ignore_ascii_case("quit") {
             break;
         }
+    }
+}
+
+fn run_file(file_path: &str) {
+    let mut file =
+        File::open(file_path).expect(format!("Could not open file {}", file_path).as_str());
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents)
+        .expect("Could not write file to string");
+
+    println!("{}", contents);
+
+    // run the file
+}
+
+fn main() {
+    let use_repl = false;
+
+    if use_repl {
+        repl()
+    } else {
+        run_file("/Users/dj/Code/rlox/data/test.rlox")
     }
 }
